6e5cf869ecd1124b447af3997ce1199d416b5e89
Dawid Rogowicz <rogowiczdawid@gmail.com>
2020-12-01 20:21:38 +0100

    Initial commit

---
+++ discord_bot.py
@@ -0,0 +1,29 @@
+import discord
+import tenorpy
+from os import getenv
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+
+BOT_TOKEN=getenv('BOT_TOKEN')
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='UwU ',intents=intents)
+tenor = tenorpy.Tenor()
+
+
+@bot.event
+async def on_ready():
+    print('bot ready')
+
+@bot.event
+async def on_member_join(member):
+    for channel in member.guild.text_channels:
+        if channel.name == 'welcome':
+            await channel.send(f'Welcome {member.mention}! UwU')
+            await channel.send(tenor.random('anime welcome'))
+
+bot.run(BOT_TOKEN)
